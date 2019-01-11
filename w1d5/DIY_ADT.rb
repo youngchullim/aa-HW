6a1aaac3fc811aad31b20dd690809744bdd7fcb7
Young Chul Lim <youngchullim95@gmail.com>
2019-01-10 21:26:37 -0800

    Exercise 3 Map

--- w1d5/DIY_ADT.rb
+++ w1d5/DIY_ADT.rb
@@ -7,10 +7,13 @@ class Stack
 
   def push(el)
     self.stack += [el]
+    nil
   end
 
   def pop
+    removed = self.stack[-1]
     self.stack -= [self.stack[-1]]
+    return removed
   end
 
   def peek
@@ -20,19 +23,63 @@ end
 
 class Queue
   attr_accessor :queue
+
   def initialize
     @queue = []
   end
 
   def enqueue(el)
     self.queue += [el]
+    nil
   end
 
   def dequeue
-    self.queue -= [self.queue[0]]
+    removed = self.queue[0]
+    self.queue -= [removed]
+    return removed
   end
 
   def peek
     self.queue[0]
+  end
+end
+
+class Map
+  attr_accessor :mapp
+
+  def initialize
+    @mapp = []
+  end
+
+  def set(key, val)
+    self.mapp << [key,val] if self.mapp.empty?
+
+    self.mapp.each do |arr|
+      arr[1] = val if arr[0] == key
+      self.mapp << [key,val] if self.mapp.none? {|sub_arr| sub_arr[0] == key}
+    end
+    nil
+  end
+
+  def get(key)
+    self.mapp.each do |arr|
+      return arr if arr[0] == key
+    end
+    nil
+  end
+
+  def delete(key)
+    selected = self.mapp.select {|arr| arr[0] == key}
+    removed = selected.flatten
+    if selected.length < 1
+      return nil
+    else
+      self.mapp -= [removed]
+      return removed
+    end
+  end
+
+  def show
+    self.mapp
   end
 end
